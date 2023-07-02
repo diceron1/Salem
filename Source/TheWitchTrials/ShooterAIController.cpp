d48d4897af1fe51b3f763b560d752abcba2df63e
diceron1 <rdice.3d@gmail.com>
2023-07-02 14:52:32 -0400

    C++ Script import

---
+++ Source/TheWitchTrials/ShooterAIController.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAIController.h"
+#include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+void AShooterAIController::BeginPlay()
+{
+    Super::BeginPlay();
+
+     if(AIBehavior != nullptr)
+    {
+          RunBehaviorTree(AIBehavior);
+
+          APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+                   
+          GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
+
+
+    }
+}
+
+void AShooterAIController::Tick(float DeltaSeconds)
+{     
+    Super::Tick(DeltaSeconds);
+}
